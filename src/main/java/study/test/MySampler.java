3b06098549b8ef22417f59adccc69f7d675b786e
wangzhenjun <wangzhenjun@okay.cn>
2018-11-26 13:44:28 +0800

    更新代码

--- src/main/java/study/test/MySampler.java
+++ src/main/java/study/test/MySampler.java
@@ -44,6 +44,7 @@ public class MySampler extends AbstractJavaSamplerClient {
         Arguments params = new Arguments();
         params.addArgument("num1", "");
         params.addArgument("num2", "");
+        params.addEmptyArgument();
         return params;
     }
 

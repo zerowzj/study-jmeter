2719117f9c5a8a8ccb25726deb8840f8dc280933
wangzhenjun <wangzhenjun@okay.cn>
2018-11-26 11:31:31 +0800

    更新代码

--- src/main/java/study/test/MySampler.java
+++ src/main/java/study/test/MySampler.java
@@ -7,8 +7,6 @@ import org.apache.jmeter.samplers.SampleResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.net.URLEncoder;
-
 public class MySampler extends AbstractJavaSamplerClient {
 
     Logger LOGGER = LoggerFactory.getLogger(MySampler.class);
@@ -19,7 +17,7 @@ public class MySampler extends AbstractJavaSamplerClient {
     @Override
     public SampleResult runTest(JavaSamplerContext context) {
         SampleResult sr = new SampleResult();
-        sr.setSampleLabel("LabelLabelLabelLabelLabelLabelLabelLabel");
+        sr.setSampleLabel("SampleLabel");
         //TODO
         sr.sampleStart();
         try {
@@ -62,13 +60,6 @@ public class MySampler extends AbstractJavaSamplerClient {
      */
     @Override
     public void setupTest(JavaSamplerContext context) {
-
-    }
-
-    public static void main(String[] args) throws Exception {
-        String str = "'";
-
-        System.out.println(URLEncoder.encode(str, "UTF-8"));
 
     }
 }
